e004419ca5224146adf93b5e70726c3bdb2a03f5
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-02-08 02:08:25 +0530

    frontend is configured and js css files added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 const port = 8000;
 const app = express();
 const dataBase = require('./config/mongoose');
@@ -6,6 +7,13 @@ const dataBase = require('./config/mongoose');
 // Setting view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// Setting static files path
+app.use(express.static('./assets'));
+
+app.use(expressEjsLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // Redirecting all requests to router
 app.use('/', require('./routes'));
